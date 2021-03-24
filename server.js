4a0dd8261b70161e954968a83af84610fed51100
basimkarhabi <basim.karhabi@gmail.com>
2021-03-24 08:13:37 +0100

    FIrst-Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+//console.log("Hello World")
+
+//creatServer
+// listener that waits for request
+const server = http.createServer((requests,response)=> {
+    response.write("<h1>Hello User Basem in Node</h1>")
+    response.end()
+})
+
+//start the server/ wait for requests on prot 8080
+server.listen(8080)
